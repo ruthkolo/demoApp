f01e0b17d7399627508669f41365ea6213779a51
RuthKolo <koloruthy10@gmail.com>
2021-10-02 02:33:48 -0400

    Initial README.md

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Ruth kolo on 9/29/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
